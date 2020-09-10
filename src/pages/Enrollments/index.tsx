c05f01a3f7999791af73b4ef1b5345646c87bf4d
guilhermeulbriki <guilhermeulbriki@gmail.com>
2020-09-10 10:27:58 -0300

    enrollments page, without table yet

--- src/pages/Enrollments/index.tsx
+++ src/pages/Enrollments/index.tsx
@@ -1,14 +1,215 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { ResponsivePie } from '@nivo/pie';
+import { BounceLoader } from 'react-spinners';
+import { FiSearch, FiArrowLeft, FiArrowRight } from 'react-icons/fi';
 
-import { Container, Content } from './styles';
+import api from '../../services/api';
+import putFirstLetterUperCase from '../../utils/putFirstLetterUperCase';
 import SideMenu from '../../components/SideMenu';
+import {
+  Container,
+  Content,
+  EnrollmentData,
+  PieGraph,
+  EnrollmentTable,
+  EnrollmentTableColumn,
+  SponsorTable,
+  SponsorTableSearch,
+  SponsorTablePagination,
+} from './styles';
+
+interface IEnrollmentsGraph {
+  enrollments: number;
+  newEnrollments: number;
+  shutdowns: number;
+}
+
+interface Enrollment {
+  created_at: Date;
+  updated_at: Date;
+}
+
+interface IGroups {
+  label: string;
+  students: number;
+}
 
 const Enrollments: React.FC = () => {
+  const [enrollmentsGraph, setEnrollmentsGraph] = useState<IEnrollmentsGraph>(
+    {} as IEnrollmentsGraph,
+  );
+  const [groups, setGroups] = useState<IGroups[]>([]);
+
+  useEffect(() => {
+    async function loadData(): Promise<void> {
+      const responseEnrollment = await api.get<Enrollment[]>('/enrollments');
+      const responseShutdown = await api.get('/shutdowns');
+
+      let totalEnrollments = 0;
+      let totalUpdatedEnrollments = 0;
+
+      responseEnrollment.data.forEach((enrollment) => {
+        if (enrollment.created_at === enrollment.updated_at) {
+          totalEnrollments += 1;
+        } else {
+          totalUpdatedEnrollments += 1;
+        }
+      });
+
+      setEnrollmentsGraph({
+        enrollments: totalEnrollments,
+        newEnrollments: totalUpdatedEnrollments,
+        shutdowns: responseShutdown.data.length,
+      });
+    }
+
+    loadData();
+  }, []);
+
+  useEffect(() => {
+    interface IGroupsForEach {
+      name: string;
+      students: [];
+    }
+
+    async function loadData(): Promise<void> {
+      const groupsData = await api.get('/groups/list', {
+        params: { city: '' },
+      });
+
+      const formatedData: IGroups[] = [];
+
+      groupsData.data.forEach((group: IGroupsForEach) => {
+        formatedData.push({
+          label: group.name,
+          students: group.students.length > 0 ? group.students.length : 1,
+        });
+      });
+
+      setGroups(formatedData);
+    }
+
+    loadData();
+  }, []);
+
   return (
     <Container>
       <SideMenu />
 
-      <Content />
+      <Content>
+        <EnrollmentData>
+          <PieGraph>
+            <strong>
+              Comparação entre matrículas, rematrículas e desistências
+            </strong>
+
+            <div>
+              {enrollmentsGraph.enrollments ? (
+                <ResponsivePie
+                  data={[
+                    {
+                      id: 'matriculas',
+                      label: 'Matrículas',
+                      value: enrollmentsGraph.enrollments,
+                    },
+                    {
+                      id: 'rematriculas',
+                      label: 'Rematrículas',
+                      value: enrollmentsGraph.newEnrollments,
+                    },
+                    {
+                      id: 'desistencias',
+                      label: 'Desistencias',
+                      value: enrollmentsGraph.shutdowns,
+                    },
+                  ]}
+                  margin={{ top: 10, right: 150, bottom: 20, left: 20 }}
+                  colors={['#77CF7C', '#F4D35E', '#eb5160']}
+                  innerRadius={0.6}
+                  borderWidth={1}
+                  borderColor={{ from: 'color', modifiers: [['darker', 0.2]] }}
+                  enableSlicesLabels={false}
+                  enableRadialLabels={false}
+                  legends={[
+                    {
+                      anchor: 'right',
+                      itemsSpacing: 18,
+                      translateX: 150,
+                      direction: 'column',
+                      itemWidth: 100,
+                      itemHeight: 30,
+                      itemTextColor: '#999',
+                      symbolSize: 14,
+                      symbolShape: 'circle',
+                      effects: [
+                        {
+                          on: 'hover',
+                          style: {
+                            itemTextColor: '#4F4F4F',
+                          },
+                        },
+                      ],
+                    },
+                  ]}
+                />
+              ) : (
+                <span
+                  style={{
+                    display: 'flex',
+                    height: 150,
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                  }}
+                >
+                  <BounceLoader color="#6668D0" />
+                </span>
+              )}
+            </div>
+          </PieGraph>
+
+          <EnrollmentTable>
+            <strong>Matrículas por turma</strong>
+
+            <div>
+              <EnrollmentTableColumn>
+                <strong>Cor</strong>
+                {groups.map((group) => (
+                  <span key={group.label}>
+                    {putFirstLetterUperCase(group.label)}
+                  </span>
+                ))}
+              </EnrollmentTableColumn>
+              <EnrollmentTableColumn>
+                <strong>Quantidade</strong>
+                {groups.map((group) => (
+                  <span key={group.label}>{group.students}</span>
+                ))}
+              </EnrollmentTableColumn>
+            </div>
+          </EnrollmentTable>
+        </EnrollmentData>
+
+        <SponsorTable>
+          <header>
+            <section>
+              <strong>Associados</strong>
+
+              <SponsorTableSearch>
+                <input type="text" placeholder="Buscar" />
+                <FiSearch color="#929292" size={18} />
+              </SponsorTableSearch>
+            </section>
+
+            <SponsorTablePagination>
+              <FiArrowLeft color="#1F4A6E" size={16} />
+              <span>
+                Página: <strong>1 </strong>
+              </span>
+              <FiArrowRight color="#1F4A6E" size={16} />
+            </SponsorTablePagination>
+          </header>
+        </SponsorTable>
+      </Content>
     </Container>
   );
 };
--- src/pages/Enrollments/styles.ts
+++ src/pages/Enrollments/styles.ts
@@ -1,5 +1,4 @@
-import styled from 'styled-components';
-import { linearGradient } from 'polished';
+import styled, { keyframes } from 'styled-components';
 
 export const Container = styled.div`
   width: 100%;
@@ -9,10 +8,215 @@ export const Container = styled.div`
 `;
 
 export const Content = styled.div`
-  flex: 1;
+  width: 100%;
+  max-width: 950px;
+  margin: 0 auto;
   padding: 32px 16px 32px 32px;
-  background: ${linearGradient({
-    colorStops: ['#F2F2F2', '#E2E2E2'],
-    toDirection: 'to bottom right',
-  })};
+`;
+
+export const EnrollmentData = styled.section`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+
+  & article + article {
+    margin-left: 24px;
+  }
+`;
+
+const pieGraphAnimation = keyframes`
+  from {
+    transform: rotate(150deg);
+  } to {
+    transform: rotate(0deg);
+  }
+`;
+
+const pieGraphLegendAnimation = keyframes`
+  from {
+    opacity: 0;
+  } to {
+    opacity: 1;
+  }
+`;
+
+export const PieGraph = styled.article`
+  min-width: 380px;
+
+  strong {
+    color: #00111f;
+    font-size: 14px;
+    font-weight: 500;
+  }
+
+  > div {
+    height: 240px;
+    width: 404px;
+    background: #f9f9f9;
+    border-radius: 16px;
+    border: 1px solid rgba(146, 146, 146, 0.2);
+    margin-top: 16px;
+    padding: 24px;
+    cursor: pointer;
+
+    > div div svg g g path {
+      animation: ${pieGraphAnimation} 1.5s ease-out;
+    }
+
+    div div svg g g:last-child g {
+      animation: ${pieGraphLegendAnimation} 1.5s ease-out;
+    }
+
+    transition: 0.3s;
+
+    &:hover {
+      box-shadow: 0px 0px 10px #ccc;
+      transform: scale(1.1);
+    }
+  }
+`;
+
+export const EnrollmentTable = styled.article`
+  height: 240px;
+  width: 404px;
+
+  strong {
+    color: #00111f;
+    font-size: 16px;
+    font-weight: 500;
+  }
+
+  div {
+    background: #f9f9f9;
+    border-radius: 8px;
+    border: 1px solid #c2c2c2;
+    margin-top: 16px;
+    padding: 8px 16px;
+    overflow-y: auto;
+    height: 240px;
+    transition: 0.3s;
+    cursor: pointer;
+
+    display: flex;
+
+    &:hover {
+      box-shadow: 0px 0px 10px #ccc;
+      transform: scale(1.1);
+    }
+
+    ::-webkit-scrollbar {
+      width: 4px;
+    }
+
+    ::-webkit-scrollbar-thumb {
+      background-color: #0084b9;
+      border-radius: 8px;
+    }
+
+    ::-webkit-scrollbar-track {
+      background-color: #00a3e4;
+    }
+  }
+`;
+
+export const EnrollmentTableColumn = styled.aside`
+  width: 202px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  strong {
+    margin-bottom: 16px;
+  }
+
+  span {
+    font-size: 14px;
+    color: #1a1a1a;
+    margin-bottom: 8px;
+
+    animation: ${pieGraphLegendAnimation} 1.5s ease-out;
+  }
+
+  & + aside {
+    border-left: 1px solid #c2c2c2;
+  }
+`;
+
+export const SponsorTable = styled.div`
+  background: #f9f9f9;
+  margin-top: 48px;
+  border-radius: 16px;
+  padding: 16px 24px;
+  min-height: 400px;
+
+  transition: 0.3s;
+
+  &:hover {
+    box-shadow: 0px 0px 10px #ccc;
+  }
+
+  header {
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    section {
+      display: flex;
+      align-items: center;
+      width: 100%;
+
+      strong {
+        color: #1f4a6e;
+        font-size: 16px;
+        font-weight: 600;
+        margin-right: 16px;
+      }
+    }
+  }
+`;
+
+export const SponsorTableSearch = styled.section`
+  height: 30px;
+  width: 100%;
+  max-width: 470px;
+  margin-right: 32px;
+  padding: 0 16px;
+  border-radius: 16px;
+  background: #eaeaea;
+
+  display: flex;
+  justify-content: space-between;
+
+  > input {
+    background: transparent;
+    border: 0;
+    width: 100%;
+    padding-right: 16px;
+    font-size: 14px;
+    color: #4f4f4f;
+
+    &::placeholder {
+      color: #929292;
+    }
+  }
+`;
+
+export const SponsorTablePagination = styled.aside`
+  display: flex;
+  align-items: center;
+
+  > span {
+    margin: 0 4px;
+    font-size: 14px;
+    display: flex;
+    align-items: center;
+
+    > strong {
+      margin-left: 4px;
+      color: #1f4a6e;
+      font-size: 16px;
+      font-weight: 500;
+    }
+  }
 `;
